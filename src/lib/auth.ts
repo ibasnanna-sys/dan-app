ed1e396868c167d699548a147898dbb02962b78f
ibasnanna-sys <ibas.nanna@gmail.com>
2026-05-19 14:56:20 +0800

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,14 +1,21 @@
-import { supabase } from './supabase'
-
-export async function registerUser(
-  email: string,
-  password: string
-) {
-  const { data, error } = await supabase.auth.signUp({
-    email,
-    password,
-  })
-
-  return { data, error }
+export function saveSession(member: any) {
+  localStorage.setItem(
+    "member",
+    JSON.stringify(member)
+  );
 }
 
+export function getSession() {
+  const member =
+    localStorage.getItem("member");
+
+  if (!member) return null;
+
+  return JSON.parse(member);
+}
+
+export function logoutSession() {
+  localStorage.removeItem("member");
+
+  window.location.href = "/login";
+}
